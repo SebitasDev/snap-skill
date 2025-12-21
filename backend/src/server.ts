45c3d97d6a40429f4805191530f9e3ba484e6c6c
sebastianramirezgarcua <camicarva119@gmail.com>
2025-12-21 01:28:14 -0300

    fix: move server.ts to src for proper build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { connectDB } from "./src/config/db";
-import app from "./src/app";
+import { connectDB } from "./config/db";
+import app from "./app";
 
 const PORT = process.env.PORT || 4000;
 

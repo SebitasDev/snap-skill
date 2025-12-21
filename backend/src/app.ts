3ab9303aa0a895264647919a89fafab0206eba9a
sebastianramirezgarcua <camicarva119@gmail.com>
2025-12-21 01:47:01 -0300

    fix: cors origin true for credentials

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -43,7 +43,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
